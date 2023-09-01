eadcf2f0fa26715dab0c8af250ac15b25ab3c060
yanisn25 <143808656+yanisn25@users.noreply.github.com>
2023-09-01 19:31:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TUgas1PDT</title>
+</head>
+<body>
+
+<h1>Pemrosesan Data Terdistribusi</h1>
+
+<?php
+
+$perkenalan = "Hallo, ini Yani.";
+
+// Menampilkan 
+echo "<p>$perkenalan</p>";
+?>
+
+</body>
+</html>
